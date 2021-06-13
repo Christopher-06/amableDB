5b62dfa99a9fe151caefd31a010cc222a0754cc1
Christopher-06 <63494008+Christopher-06@users.noreply.github.com>
2021-06-13 14:09:05 +0200

    Support POST-Methods

--- api.cpp
+++ api.cpp
@@ -2,20 +2,62 @@
 #include <map>
 #include <thread>
 
+#include <boost/algorithm/string/replace.hpp>
 #include <cpprest/http_listener.h>
 #include <nlohmann/json.hpp>
 #include "api.h"
 #include "database.h"
 #include "CRUD/crud.h"
 
-void HttpHandler::get(web::http::http_request& request)
+namespace Endpoints {
+	nlohmann::json POST_Index(nlohmann::json& indexData) {
+		// Iterate through Objects
+		for (const auto& item : indexData.items()) {
+			const std::string indexName = item.key();
+			const nlohmann::json indexOp = item.value();
+			const std::string colName = indexOp["collection"].get<std::string>();
+
+			if(!collections.count(colName)) // Collection does not exist
+				return nlohmann::json({ {"status", "failed"}, {"msg", "Collection does not exist"}, {"collection", colName}});
+
+			// Define index
+			if (indexOp.contains("definition") && indexOp["definition"].is_object()) {
+				if (collections[colName]->indexes.count(indexName)) // Index is already defined
+					return nlohmann::json({ {"status", "ok"}, {"msg", "Index does already exists"}, {"index", indexName} });
+
+				nlohmann::json definition = indexOp["definition"];
+				definition["name"] = indexName;
+
+				// Create Index, set and directly build it
+				DbIndex::Iindex_t* index = DbIndex::loadIndexFromJSON(definition);
+				collections[colName]->indexes[indexName] = index;
+				index->buildIt(collections[colName]->storage);
+			}
+
+			// Build Index
+			if (indexOp.contains("build") && indexOp["build"].get<bool>()) {
+				if(!collections[colName]->indexes.count(indexName)) // Index is never defined
+					return nlohmann::json({ {"status", "failed"}, {"msg", "Index does not exist. Please create one first"}, {"index", indexName} });
+				
+				collections[colName]->indexes[indexName]->buildIt(collections[colName]->storage);
+			}
+		}
+
+		return nlohmann::json({ {"status", "ok"} });
+	}
+}
+
+void HttpHandler::get(web::http::http_request request)
 {
 	request.reply(web::http::status_codes::OK, "Hello World");
 }
 
-void HttpHandler::post(web::http::http_request& request)
+void HttpHandler::post(web::http::http_request request)
 {	
-	std::wstring requestedPath = request.relative_uri().to_string().substr(0, 7);
+	auto path = request.relative_uri().to_string();
+	std::wstring requestedPath = std::wstring(path.begin(), path.end());
+	boost::replace_all(requestedPath, "/", "");
+
 	if (!apiRoutes["POST"].count(requestedPath)) {
 		// Path does not exist
 		request.reply(web::http::status_codes::NotFound, 
@@ -39,31 +81,28 @@ void defineRoutes() {
 	apiRoutes = {};
 
 	// POST Routes
-	apiRoutes["POST"][L"/create"] = CRUD::create;
-	apiRoutes["POST"][L"/select"] = CRUD::select;
-	apiRoutes["POST"][L"/update"] = CRUD::update;
-	apiRoutes["POST"][L"/remove"] = CRUD::remove;
+	apiRoutes["POST"][L"create"] = CRUD::create;
+	apiRoutes["POST"][L"select"] = CRUD::select;
+	apiRoutes["POST"][L"update"] = CRUD::update;
+	apiRoutes["POST"][L"remove"] = CRUD::remove;
+	apiRoutes["POST"][L"index"] = Endpoints::POST_Index;
+	
 }
 
 void startAPI(const int apiPort, const std::string apiAddress)
 {
 	defineRoutes();
+	
+	uri.set_scheme(U("http"));
+	uri.set_host(utility::conversions::to_string_t(apiAddress));
+	uri.set_port(utility::conversions::to_string_t(std::to_string(apiPort)));
+	uri.set_path(U("/"));
 
-	web::uri_builder uri;
-		uri.set_scheme(U("http"));
-		uri.set_host(utility::conversions::to_string_t(apiAddress));
-		uri.set_port(utility::conversions::to_string_t(std::to_string(apiPort)));
-		uri.set_path(U("/"));
-
-	web::http::experimental::listener::http_listener listener(uri.to_uri());
+	listener = web::http::experimental::listener::http_listener(uri.to_uri());
 	listener.support(web::http::methods::GET, HttpHandler::get);
 	listener.support(web::http::methods::POST, HttpHandler::post);
 
-	listener
-		.open()
-		.then([&listener, &uri]() {std::cout << "[INFO] API is up " << std::endl; })
+	listener.open()
+		.then([]() {std::cout << "[INFO] API is up at: " << uri.to_string().c_str() << std::endl; })
 		.wait();
-	
-
-	while (true);
 }
--- api.h
+++ api.h
@@ -2,14 +2,23 @@
 #define API_H
 
 #include <string>
+#include <thread>
+#include <map>
 #include <cpprest/http_listener.h>
+#include <nlohmann/json.hpp>
 
+inline web::uri_builder uri;
+inline web::http::experimental::listener::http_listener listener;
 inline std::thread apiRunner;
-inline std::map<std::string, std::map<std::wstring, std::function<nlohmann::json(nlohmann::json)>>> apiRoutes;
+inline std::map<std::string, std::map<std::wstring, std::function<nlohmann::json(nlohmann::json&)>>> apiRoutes;
+
+namespace Endpoints {
+	nlohmann::json POST_Index(nlohmann::json&);
+}
 
 namespace HttpHandler {
-	void get(web::http::http_request&);
-	void post(web::http::http_request&);
+	void get(web::http::http_request);
+	void post(web::http::http_request);
 }
 
 void defineRoutes();
